3f2d146767d3c9b62ed9c16251d9c9aba5658727
Astrid Schindler <astrid.schindler91@icloud.com>
2020-12-14 14:41:42 +0100

    Datentypen kennengelernt

---
+++ 2_First-Steps/04-strict-mode.js
@@ -0,0 +1,7 @@
+// "use strict" ignoriert alten JavaScript-Code und wirft Fehler falls er
+// benutzt wird - wird jedoch nur von modernen Umgebungen unterstützt
+"use strict"
+
+greeting = "Hey dude!";
+
+console.log(greeting);
