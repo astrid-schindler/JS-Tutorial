d75437ca43ff591383931f131847dc85527d8874
Astrid Schindler <astrid.schindler91@icloud.com>
2020-12-14 17:13:51 +0100

    Weitere nützliche String-Funktionen

---
+++ 3_Weitere-Grundlagen/02_strings2.js
@@ -0,0 +1,13 @@
+"use strict"
+
+// Newline
+console.log('Hej Dude!\nIch hoffe dir geht es gut?')
+
+// String wiederholen
+console.log("-".repeat(20))
+
+// .slice(start, end)
+console.log("Hallo Dude!".slice(6,10))
+
+// .substr(start, length)
+console.log("Hallo Babe!".substr(6, 2))
