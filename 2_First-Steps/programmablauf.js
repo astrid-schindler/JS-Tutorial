56932f5a511f3702d50fb93781f721ec648fa379
Astrid Schindler <astrid.schindler91@icloud.com>
2020-12-14 14:05:33 +0100

    Definition einer Variablen

--- 2_First-Steps/programmablauf.js
+++ 2_First-Steps/programmablauf.js
@@ -1,6 +1,9 @@
+// Variable "greeting" wird erstellt
 let greeting
 
+// Der Variablen "greeting" wird ein Wert zugewiesen
 greeting = "Hello Welt!"
 
+// Die Variable "greeting" wird 2x ausgegeben
 console.log(greeting);
 console.log(greeting);
